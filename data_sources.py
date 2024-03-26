bf218a3a7645a9432e01fc77c2e8c5d67fa1d4f5
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2024-03-26 17:14:14 +0000

    UKB PPP: Ingest study index

--- data_sources.py
+++ data_sources.py
@@ -12,26 +12,23 @@ from spark_prep import SparkPrep
 @dataclass
 class EqtlCatalogue(DataSourceBase):
     """A dataclass for ingesting the eQTL Catalogue."""
+
     data_source_name = "eqtl_catalogue"
     study_index_source = "https://raw.githubusercontent.com/eQTL-Catalogue/eQTL-Catalogue-resources/master/tabix/tabix_ftp_paths_imported.tsv"
 
     def ingest_study_index(self):
-        """Ingest study index and store it in a remote location."""
+        # Read the original metadata table.
         study_index = pd.read_table(self.study_index_source)
+        # Save the study index with no modifications.
         study_index.to_csv(self._get_study_index_location(), sep="\t", index=False)
 
     def ingest_single_summary_stats(self, task_index: int) -> None:
-        """Ingest a single study from the data source.
-
-        Args:
-            task_index (int): The index of the current study being ingested across all studies in the study index.
-        """
         # Read the study index and select one study.
         record = self._get_study_index().loc[task_index].to_dict()
         qtl_group = record["qtl_group"]
         # Process the study.
         worker = SparkPrep(
-            number_of_cores = self.cpu_per_task,
+            number_of_cores=self.cpu_per_task,
             input_uri=record["ftp_path"],
             separator="\t",
             chromosome_column_name="chromosome",
@@ -41,5 +38,66 @@ class EqtlCatalogue(DataSourceBase):
         worker.process()
 
 
-all_data_source_classes = [EqtlCatalogue]
+@dataclass
+class UkbPppEur(DataSourceBase):
+    """A dataclass for ingesting UKB PPP (European)."""
+
+    data_source_name = "ukb_ppp_eur"
+    study_index_source = "gs://gentropy-vault/ukb-ppp/Metadata/Protein annotation/olink_protein_map_3k_v1.tsv"
+    summary_stats_prefix = "gs://gentropy-vault/ukb-ppp/UKB-PPP pGWAS summary statistics/European (discovery)"
+
+    def _process_study_index(self, row) -> tuple[str, str]:
+
+        # Create link to summary stats.
+        # Example: CLEC4D:Q8WXI8:OID20609:v1 -> CLEC4D_Q8WXI8_OID20609_v1.
+        converted_id = row["UKBPPP_ProteinID"].replace(":", "_")
+        # Example: CLEC4D_Q8WXI8_OID20609_v1_Inflammation.tar.
+        summary_stats_filename = f"{converted_id}_{row['Panel']}.tar"
+        # Example: gs://gentropy-vault/ukb-ppp/UKB-PPP pGWAS summary statistics/European (discovery)/
+        summary_stats_link = f"{self.summary_stats_prefix}/{summary_stats_filename}"
+
+        # Manual case 2: recreate study ID. This is necessary because some studies contain a measurement of a mixture of
+        # proteins and need to be clarified to avoid ID duplication in the final study index. See detailed discussions
+        # in https://github.com/opentargets/issues/issues/3234. Example: UKB_PPP_EUR_AMY1B_P0DTE7_OID30707_v1.
+        study_id = "_".join(
+            [
+                "UKB_PPP_EUR",
+                row["HGNC.symbol"],
+                row["UniProt2"],
+                row["OlinkID"],
+                "v1",
+            ]
+        )
+
+        return study_id, summary_stats_link
+
+    def ingest_study_index(self) -> None:
+        # Read the original metadata table.
+        study_index = pd.read_table(self.study_index_source)
+        # Manual case 1: exactly one study does not correspond to any available summary stats files.
+        study_index = study_index[
+            study_index["UKBPPP_ProteinID"] != "GLIPR1:P48060:OID31517:v1"
+        ]
+        # Process study index row by row.
+        (
+            study_index["_gentropy_study_id"],
+            study_index["_gentropy_summary_stats_link"],
+        ) = zip(*study_index.apply(self._process_study_index, axis=1))
+        # Save the study index.
+        study_index.to_csv(self._get_study_index_location(), sep="\t", index=False)
+
+    def ingest_single_summary_stats(self, task_index: int) -> None:
+        # Read the study index and select one study.
+        record = self._get_study_index().loc[task_index].to_dict()
+
+        return
+
+        # Process the study.
+        worker = SparkPrep(
+            # TODO
+        )
+        worker.process()
+
+
+all_data_source_classes = [EqtlCatalogue, UkbPppEur]
 data_source_look_up = {c.data_source_name: c for c in all_data_source_classes}
